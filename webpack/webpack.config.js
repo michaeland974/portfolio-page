b2fb8161fd5850e7ea9502ecdd2d02108b2e42be
michaeland974 <micanderson29@gmail.com>
2023-06-21 00:40:16 -0500

    Webpack config set up

---
+++ webpack/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+   mode: "production",
+   entry: {
+      index: path.resolve(__dirname, "..", "src", "index.ts"),
+   },
+   output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "[name].js",
+      clean: true,
+   },
+   resolve: {
+      extensions: [".ts", ".js"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.ts?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
+         {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+      ],
+   },
+   plugins: [
+      new CopyPlugin({
+         patterns: [
+          {from: ".", to: ".", context: "assets"},
+          {from: "src/styles", to: "./styles"},
+          {from: "src/index.html", to: "."}
+        ]
+      })
+   ],
+   watchOptions: {
+    aggregateTimeout: 600,
+    ignored: '**/node_modules'
+  },
+};
